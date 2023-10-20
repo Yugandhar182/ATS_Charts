721c07efd8a003c801d090ec54ef3609a6442c1a
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-20 11:52:14 +0530

    Update DateRangePicker.svelte

--- src/DateRangePicker.svelte
+++ src/DateRangePicker.svelte
@@ -11,21 +11,26 @@
   const lastYearStartMonth = `${currentYear - 1}-${currentMonth.toString().padStart(2, '0')}`;
   const currentMonthDate = `${currentYear}-${currentMonth.toString().padStart(2, '0')}`;
 
+  // Initialize startDate and endDate with values from local storage
   let startDate = localStorage.getItem('startDate') || lastYearStartMonth;
   let endDate = localStorage.getItem('endDate') || currentMonthDate;
+
+  let formattedStartDate = formatStoredDate(startDate);
+  let formattedEndDate = formatStoredDate(endDate);
 
   onMount(() => {
     initializeDateRangePicker(startDate, endDate);
   });
 
   afterUpdate(() => {
-    // After the component updates, check if the local storage values have changed
     const updatedStartDate = localStorage.getItem('startDate') || '';
     const updatedEndDate = localStorage.getItem('endDate') || '';
 
     if (startDate !== updatedStartDate || endDate !== updatedEndDate) {
       startDate = updatedStartDate;
       endDate = updatedEndDate;
+      formattedStartDate = formatStoredDate(updatedStartDate);
+      formattedEndDate = formatStoredDate(updatedEndDate);
     }
   });
 
@@ -59,46 +64,52 @@
     const year = date.getFullYear();
     return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
   }
+
+  function formatStoredDate(date) {
+    const [day, month, year] = date.split('/');
+    const monthName = new Date(`${year}-${month}-01`).toLocaleDateString('default', { month: 'short' });
+    return `${monthName} ${year}`;
+  }
 </script>
 
 <div class="calendar-container">
   <div class="col-lg-12 control-section">
     <div id="wrapper">
-      <input id="element" type="text" /><br/><br/>
+      <input id="element" type="text" value="{formattedStartDate} - {formattedEndDate}"/>
     </div>
   </div>
 </div>
+
 <style>
 
-.calendar-container {
-    position: absolute;
-    top: 30px; /* Adjust the top position as needed */
-    right: 15px; /* Adjust the left position as needed */
-    width: 300px;
-    height: 60px;
-    text-align: center;
-    align-items: center; /* Center content horizontally */
-
-  }
-#element{
-    width :210px;
-    height: 40px;
-    border-color: #ced4da !important;
-    background: linear-gradient(180deg,#fff,#f6f7f9) !important;
-    transition: box-shadow .2s ease-in-out,border-color .2s ease-in-out !important;
-    box-shadow: inset 0 1px 0 0 rgba(34,34,48,.05);
-    border: 1px solid;
-    border-width: 1px;
-    margin: 10PX;
-    position: relative; 
-    font-size: 16px;
-    font-weight: 400px;
-    font-family: Arial, sans-serif;
-    color: #333;
-    top:0px;
-    border-radius: 0.25rem;
- 
-  }
-
-
+  .calendar-container {
+      position: absolute;
+      top: 30px; 
+      right: 15px; 
+      width: 300px;
+      height: 60px;
+      text-align: center;
+      align-items: center; 
+  
+    }
+    
+  #element{
+      width :210px;
+      height: 40px;
+      border-color: #ced4da ;
+      background: linear-gradient(180deg,#fff,#f6f7f9) ;
+      transition: box-shadow .2s ease-in-out,border-color .2s ease-in-out ;
+      box-shadow: inset 0 1px 0 0 rgba(34,34,48,.05);
+      border: 1px solid;
+      border-width: 1px;
+      margin: 10PX;
+      position: relative; 
+      font-size: 16px;
+      font-weight: 400px;
+      font-family: Arial, sans-serif;
+      color: #333;
+      top:0px;
+      border-radius: 0.25rem;
+   
+    }
 </style>
