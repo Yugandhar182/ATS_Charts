090164a08d80325df0e11b6930267c8d012c84ff
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-20 12:30:18 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,27 @@
 <script>
-
-
     export let appData; // Export appData from main.svelte
     import Calendar from './DateRangePicker.svelte';
     import Metric from './Metric.svelte';
     import Time from "./TimeToFill.svelte"
     import Placement from "./PlacementByUser.svelte"
     import Reject from "./RejectedReasons.svelte"
-   </script>
+
+</script>
   
- 
+ <body>
     <Calendar/>
     <Metric {appData}/>
     <Time {appData}/>
     <Placement {appData}/>
     <Reject {appData}/>
-    
+</body>
  
     <style>
-      
+            body{
+                background-color: #f6f7f9;
+                height: 1900px;
+                margin: 0px;
+                margin-top:-40px
+             }
+ 
     </style>
